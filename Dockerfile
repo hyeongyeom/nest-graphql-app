4a34a42c7ac56b1bd431b699592924bf1c92bfaf
hyeongyeom <wltnwkd244@naver.com>
2022-10-16 18:18:41 +0900

    feature: deploy ecs through ecr

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# builder pattern
+FROM node:16.14 AS builder
+
+WORKDIR /app
+
+
+COPY . .
+
+
+RUN yarn install
+RUN yarn build
+
+
+FROM node:16.14-alpine3.14
+
+WORKDIR /app
+
+COPY --from=builder /app ./
+
+EXPOSE 4000
+
+CMD ["yarn", "start"]
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as path from 'path';
+import dotenv from 'dotenv'
 
+dotenv.config({
+  path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
+})
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
   username: 'postgres',
-  password: '123123',
+  password: process.env.DB_PASSWORD,
   database: 'nest-graphql',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
